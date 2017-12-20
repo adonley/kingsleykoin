00444ee0e45566b9dca21ca8500952c48f9fb3b0
Andrew Donley <andrewjdonley@gmail.com>
2017-12-20 03:58:28 -0800

    added some copy and styles

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # Kingsleykoin
 
+A joke token demonstrating how to integrate web3 into angular 2+.
+
+### Requirements
+
+* truffle
+* angular-cli
+
 ### Installation
 ```
-sudo npm i -g @angular/cli && npm install
+sudo npm i -g @angular/cli
+npm install
+```
+
+### Running
+```
+ng serve
 ```
 
 ### License
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import {SharedModule} from "../shared/shared.module";
 import {PublicRoutingModule} from "./public.routing";
 import {RouterModule} from "@angular/router";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import {RouterModule} from "@angular/router";
   ],
   declarations: [
     HomeComponent,
+    NavbarComponent
   ]
 })
 export class PublicModule { }
